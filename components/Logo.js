a14120f810dca3725d23bd776a32a45fc817928a
FrozenCorn2113 <brettlc2113@gmail.com>
2026-02-04 10:34:04 -0800

    Replace site logo with new SVG asset

--- components/Logo.js
+++ components/Logo.js
@@ -6,7 +6,7 @@ const Logo = ({ variant = 'default', ariaLabel = 'Victoria Flooring Outlet Home'
     <Link href="/" aria-label={ariaLabel} className="flex items-center justify-center group">
       <div className="relative w-72 h-20 md:w-96 md:h-24 lg:w-[32rem] lg:h-28 xl:w-[36rem] xl:h-32 flex-shrink-0">
         <Image
-          src="/images/Victoriaflooringoutlet_logo.png"
+          src="/images/vfo-logo-final.svg"
           alt="Victoria Flooring Outlet Logo"
           fill
           className="object-cover object-[center_45%]"
